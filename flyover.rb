55eda2c83e2a21efee8bc0c0e0775fcdd80ea960
Tomas Molina <tamolinamendoza@gmail.com>
2016-06-22 04:03:36 +0000

    Done.

--- flyover.rb
+++ flyover.rb
@@ -1,1 +1,11 @@
 ## Code your solution here.
+def fasten_seatbelts
+  "Passengers we have encountered some turbulence, please " + "fasten seatbelts!".upcase
+end
+
+def free_to_move_about_the_cabin
+  "Passengers you are free to move about the cabin, but please remember you must fasten your seatbelts when in your seats."
+end
+
+puts free_to_move_about_the_cabin
+puts fasten_seatbelts
